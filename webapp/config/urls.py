2871c29202832dfc9c63097af875ae19416bf743
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2015-12-05 15:52:11 -0600

    Update base urls in config/urls.py
    * Change names to login and logout url.
    * Add staff namespace

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -7,8 +7,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^', include('apps.student.urls')),
-    url(r'^staff/', include('apps.staff.urls')),
-    url(r'^login/$', apps.core.views.login_view, name='login_url'),
-    url(r'^logout/$', apps.core.views.logout_view, name='logout_url'),
+    url(r'^staff/', include('apps.staff.urls', namespace='staff')),
+    url(r'^login/$', apps.core.views.login_view, name='login'),
+    url(r'^logout/$', apps.core.views.logout_view, name='logout'),
     # url(r'^admin/', include(admin.site.urls), name='djadmin'),
 ]
