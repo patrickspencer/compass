585cac3c19a36979fdfbd9892bb060146676391a
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2015-12-05 15:46:53 -0600

    Update users_new_view
    * Update success message
    * Redirect to users_home page

--- webapp/apps/staff/views.py
+++ webapp/apps/staff/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from apps.staff.forms import NewUserForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth.models import User
 
 
 def index_view(request):
@@ -15,7 +14,6 @@ def users_new_view(request):
     if request.method == 'POST':
         form = NewUserForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
             email = form.cleaned_data['email']
             if User.objects.filter(email=email).exists():
                 messages.add_message(request, messages.ERROR, 'Username with\
@@ -30,9 +28,9 @@ def users_new_view(request):
                         )
                 # messages = "User created"
                 messages.add_message(request, messages.SUCCESS, 'User create!.')
+                return redirect(reverse('staff:users_home'))
     else:
         form = NewUserForm()
-    messages.add_message(request, messages.INFO, 'This is a message!.')
     return render(request, 'staff/users/new.jinja', {
         'form': form,
     })
