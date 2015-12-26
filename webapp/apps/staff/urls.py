3f1a83f0c887a5ed16b2b74136674fd69993c591
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2015-12-26 16:44:57 -0500

    Update staff urls

--- webapp/apps/staff/urls.py
+++ webapp/apps/staff/urls.py
@@ -3,12 +3,12 @@ import apps.staff.views
 
 # These urls are included in apps.core.urls
 # so they are relative to /staff. For example
-# admin_home_url is set to / but it's accesible
-# at /staff/
+# users_home is set to ^users$ but it's accessible
+# at /staff/users/
 
 urlpatterns = [
-    url(r'^users/$', apps.staff.views.Index.as_view(), name='users_home'),
-    url(r'^users/new/$', apps.staff.views.Create.as_view(), name='users_new'),
+    url(r'^users$', apps.staff.views.Index.as_view(), name='users_home'),
+    url(r'^users/create$', apps.staff.views.Create.as_view(), name='users_create'),
     url(r'^users/(?P<pk>[0-9]+)/delete$',
       apps.staff.views.Delete.as_view(), name='users_delete'),
     url(r'^users/(?P<pk>[0-9]+)/update$',
