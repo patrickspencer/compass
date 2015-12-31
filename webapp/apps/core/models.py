eb1a9bd43a1d00588c1266b8bb4cbe4c8face7ca
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2015-12-31 17:06:11 -0500

    Add ProblemMapping model

--- webapp/apps/core/models.py
+++ webapp/apps/core/models.py
@@ -1,12 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
-
-class User(AbstractUser):
-    class Meta:
-        db_table = 'users'
+# Order of the classes is important
 
 class Problem(models.Model):
     value = models.TextField()
@@ -14,16 +10,11 @@ class Problem(models.Model):
     class Meta:
         db_table = 'problems'
 
-class ProblemMapping(models.Model):
-    user_id = models.ForeignKey(User)
-    problem_id = models.ForeignKey(Assignment)
-    seed = models.PositiveSmallIntegerField()
-    # assignment_id = models.ForeignKey(Assignment)
-    # assignment_order = models.ForeignKey(Assignment)
+class User(AbstractUser):
+    problems = models.ManyToManyField(Problem, through='ProblemMapping')
 
     class Meta:
-        db_table = 'problem_mappings'
-
+        db_table = 'users'
 
 class Assignment(models.Model):
     name = models.CharField(max_length=200)
@@ -36,3 +27,20 @@ class Assignment(models.Model):
     def __unicode__(self):
         return self.name
 
+class ProblemMapping(models.Model):
+    user = models.ForeignKey(User)
+    problem = models.ForeignKey(Problem)
+    seed = models.PositiveSmallIntegerField()
+    # assignment_id = models.ForeignKey(Assignment)
+    # assignment_order = models.ForeignKey(Assignment)
+
+    class Meta:
+        db_table = 'problem_mappings'
+
+class Answer(models.Model):
+    value = models.TextField()
+    user = models.ForeignKey(User)
+    problem = models.ForeignKey(Problem)
+
+    class Meta:
+        db_table = 'answers'
