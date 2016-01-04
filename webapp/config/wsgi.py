2a5479970593cea015308073887896cc22a7e1fa
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2016-01-04 01:37:30 -0800

    Add site-packages to wsgi file

--- webapp/config/wsgi.py
+++ webapp/config/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/var/compass/webapp')
+sys.path.append('/virtualenv/compass/lib/python2.7/site-packages')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
 
 from django.core.wsgi import get_wsgi_application
