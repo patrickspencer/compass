c95e25f0f812d1079f8383745dc8247d71a0d3cc
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2016-01-04 01:00:18 -0800

    Add base url

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.contrib.auth.views import login, logout
 import apps.core.views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    url(r'^$',RedirectView.as_view(url='/student/problems/', permanent=False), name='index'),
     url(r'^student/', include('apps.student.urls', namespace='student')),
     url(r'^staff/', include('apps.staff.urls', namespace='staff')),
     url(r'^login/$', apps.core.views.Login.as_view(), name='login'),
